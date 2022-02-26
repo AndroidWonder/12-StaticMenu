e0828f65bc97b5f8aac60b3f8b5a96a744d28dce
Android Wonder <jim.pepe@verizon.net>
2022-02-26 12:01:40 -0500

    add <queries> in Manifest. Migrate to Androidx

--- app/src/main/java/com/course/example/staticmenu/MainActivity.java
+++ app/src/main/java/com/course/example/staticmenu/MainActivity.java
@@ -1,9 +1,9 @@
 package com.course.example.staticmenu;
 
-import android.support.v7.app.ActionBar;
+import androidx.appcompat.app.ActionBar;
 import android.content.Intent;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
